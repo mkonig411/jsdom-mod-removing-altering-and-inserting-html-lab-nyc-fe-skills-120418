f11c9a9b652726d9d391c43b88ebafab956919fa
Margot Konig <mkonig411@gmail.com>
2019-01-11 01:24:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,11 +4,3 @@
 const newHeader = null;
 
 
-
-* Remove the `main` element.
-* Add an H1 with `id` of `victory` programmatically, using JavaScript. Inside
-  the tag you should put "(your name) is the champion!"
-
-Inside `index.js`, you'll see JavaScript comments telling you where to put the
-code. Run the tests with `learn`. When they pass, use `learn submit` to move
-on! Congratulations.
