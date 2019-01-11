c8d3bb6f8ed2dc90aabbe8349ae2b4164f6c8f7a
Margot Konig <mkonig411@gmail.com>
2019-01-11 01:25:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 document.remove(main)
+const victory = document.body.createH1
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
 
