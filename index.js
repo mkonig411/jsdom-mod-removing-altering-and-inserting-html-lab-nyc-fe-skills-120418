c6cf782735f4657671d00d8fadf740b22a04d122
Margot Konig <mkonig411@gmail.com>
2019-01-11 01:26:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* Enter the code to remove the main node element under this comment */
 document.remove(main)
-const victory = document.body.createH1
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.body.createH1('Margot is the champion!')
 
 
