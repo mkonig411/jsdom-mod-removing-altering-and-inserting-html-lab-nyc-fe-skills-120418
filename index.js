0b6dd85eee70ba1618a5416addf1e121bbcea5f2
Margot Konig <mkonig411@gmail.com>
2019-01-11 01:24:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+
+
+
+* Remove the `main` element.
+* Add an H1 with `id` of `victory` programmatically, using JavaScript. Inside
+  the tag you should put "(your name) is the champion!"
+
+Inside `index.js`, you'll see JavaScript comments telling you where to put the
+code. Run the tests with `learn`. When they pass, use `learn submit` to move
+on! Congratulations.
