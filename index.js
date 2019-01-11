4001b201aa3d1416042bccb843af869589b36d55
Margot Konig <mkonig411@gmail.com>
2019-01-11 01:27:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 document.remove(main)
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = document.body.createH1('Margot is the champion!')
+const newHeader = document.body.createH1
+newHeader.innerHTML = 'Margot is the champion!'
 
 
